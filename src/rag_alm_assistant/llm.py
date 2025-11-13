33695d11b7b79fd4427327de2182b3b636755073
Remi_Nollet <remi.nollet@live.fr>
2025-11-13 09:20:45 +0000

    minor corrections

--- src/rag_alm_assistant/llm.py
+++ src/rag_alm_assistant/llm.py
@@ -1,7 +1,7 @@
 from functools import lru_cache
 import torch
 
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline, AutoConfig, BitsAndBytesConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline, AutoConfig
 from langchain_community.llms import HuggingFacePipeline
 
 from .constants import LLM_MODEL_ID, LLM_MAX_NEW_TOKENS, LLM_DO_TOKENS, HF_TOKEN
@@ -16,18 +16,18 @@ def get_tokenizer_and_model(model_id: str = LLM_MODEL_ID):
     tokenizer = AutoTokenizer.from_pretrained(model_id, token=HF_TOKEN)
     cfg = AutoConfig.from_pretrained(model_id, token=HF_TOKEN)
 
-    bnb_config = BitsAndBytesConfig(
-        load_in_4bit=True,
-        bnb_4bit_use_double_quant=True,
-        bnb_4bit_quant_type="nf4",
-        bnb_4bit_compute_dtype=torch.float16,
-    )
+    # bnb_config = BitsAndBytesConfig(
+    #     load_in_4bit=True,
+    #     bnb_4bit_use_double_quant=True,
+    #     bnb_4bit_quant_type="nf4",
+    #     bnb_4bit_compute_dtype=torch.float16,
+    # )
     model = AutoModelForCausalLM.from_pretrained(
         model_id,
         trust_remote_code=True,
         device_map="auto",
         config=cfg,
-        quantization_config=bnb_config,
+        # quantization_config=bnb_config,
         token=HF_TOKEN
     )
     return tokenizer, model
