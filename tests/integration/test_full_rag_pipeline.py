016cc9f011faed3136760428e712a40c58480dd8
Kraligan <remi.nollet@live.fr>
2025-11-18 18:49:54 +0900

    Add integration test (full rag pipeline) with evaluation on Bert F1 scrore (>60%)

--- tests/integration/test_full_rag_pipeline.py
+++ tests/integration/test_full_rag_pipeline.py
@@ -1,2 +1,5 @@
-# TO DO after it works
-# Test F1Bert score >60%
+from rag_alm_assistant.evaluation import run_evaluation
+
+def test_pipeline_f1_score():
+    f1 = run_evaluation(n_samples=10)
+    assert f1 > 0.6
